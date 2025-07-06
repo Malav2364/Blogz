f827d7e77924817491068e13637e0ae605443807
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2025-07-06 17:53:34 +0530

    Login signup backend done

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+const authRoutes = require('./routes/authRoutes.js');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
